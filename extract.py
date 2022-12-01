bd6156bcd703f0e0a45b0f7bbc6bf2f9166a3433
Las02 <97394529+Las02@users.noreply.github.com>
2022-12-01 11:54:06 +0100

    Update extract.py

--- extract.py
+++ extract.py
@@ -4,6 +4,7 @@ import streamlit as st
 st.write("'AD' TO 'A pp D'")
 protein_list = st.text_input("Pase in your proteins in oneletter words eg: AD CP AG CD")
 pp_protein = ""
+is_error = False
 for protein in protein_list.split():
     try:
         if len(protein) != 2:
@@ -11,5 +12,6 @@ for protein in protein_list.split():
         pp_protein += (f"{protein[0]} pp {protein[1]}\n")      
     except Exception:
         st.error("One or more of the input is not of two chars")
-    
-st.code(pp_protein)
+        is_error = True
+if not is_error:
+    st.code(pp_protein)
