ce97c44676953b0968a6c7b1dbea6cc364cb7472
Las02 <97394529+Las02@users.noreply.github.com>
2022-12-01 11:48:40 +0100

    Update extract.py

--- extract.py
+++ extract.py
@@ -5,5 +5,11 @@ st.write("'AD' TO 'A pp D'")
 protein_list = st.text_input("Pase in your proteins in oneletter words eg: AD CP AG CD")
 pp_protein = ""
 for protein in protein_list.split():
-    pp_protein += (f"{protein[0]} pp {protein[1]}\n")    
+    try:
+        if len(protein) != 2:
+            raise Exception()
+  
+    except Exception:
+        st.write("One or more of the input is not of two chars")
+    pp_protein += (f"{protein[0]} pp {protein[1]}\n")  
 st.code(pp_protein)
